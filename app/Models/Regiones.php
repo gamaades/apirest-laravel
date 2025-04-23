5fccf1bd27f39f4f8ed87ffec6f5ebd3cd1f93e9
GmananOmen <gamaades@gmail.com>
2025-04-23 07:32:48 -0400

    Creando modelos y controladores de Clientes

---
+++ app/Models/Regiones.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Regiones extends Model
+{
+    // Conectandonos a la tabla region
+    protected $table = 'Regiones';
+}
